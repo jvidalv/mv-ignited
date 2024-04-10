3503230b8f309488f60e5bc89382d939f6cf82c8
jvidalv <josepvidalvidal@gmail.com>
2024-04-10 19:45:41 +0200

    Improve dark theme detection

--- src/injected/utils/theme.ts
+++ src/injected/utils/theme.ts
@@ -5,8 +5,17 @@ export const injectTheme = () => {
   const isDark = document.querySelector(
     'link[href="https://mediavida.b-cdn.net/style/429/dark_v7.css"]',
   );
+  const isFire = document.querySelector(
+    'link[href="https://mediavida.b-cdn.net/style/429/fire.css"]',
+  );
+  const isGhost = document.querySelector(
+    'link[href="https://mediavida.b-cdn.net/style/429/ghost.css"]',
+  );
+  const isEmpire = document.querySelector(
+    'link[href="https://mediavida.b-cdn.net/style/429/empire.css"]',
+  );
 
-  if (isLotr || isDark) {
+  if (isLotr || isDark || isFire || isGhost || isEmpire) {
     const html = document.getElementsByTagName("html").item(0);
 
     html?.setAttribute("class", "dark");
