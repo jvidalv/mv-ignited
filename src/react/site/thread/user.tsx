32ecb36eeaf77aac94567fb55a7c5baa23eea227
jvidalv <josepvidalvidal@gmail.com>
2024-04-11 11:13:36 +0200

    Improved visuals on ignore button

--- src/react/site/thread/user.tsx
+++ src/react/site/thread/user.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useStore } from "../../../utils/store";
+import { Tooltip } from "../components/ui";
 
 export const UserActionsInThread = ({ username }: { username: string }) => {
   const { usersIgnored, update } = useStore();
@@ -15,16 +16,19 @@ export const UserActionsInThread = ({ username }: { username: string }) => {
   };
 
   return (
-    <div className="text-left mt-2 opacity-75 hover:opacity-100 transition duration-200">
-      <button
-        title={isIgnored ? "Designorar usuario" : "Ignorar usuario"}
-        className="bg-surface-high border dark:border-gray-700 hover:bg-opacity-75 transition duration-100 rounded px-1.5 py-0.5 text-center"
-        onClick={onIgnoreUserClick}
-      >
-        <i
-          className={isIgnored ? "fa fa-microphone" : "fa fa-microphone-slash"}
-        ></i>
-      </button>
+    <div className="text-left mt-2 opacity-50 hover:opacity-100 transition duration-200">
+      <Tooltip content={isIgnored ? "Designorar usuario" : "Ignorar usuario"}>
+        <button
+          className="transition duration-100 rounded text-center"
+          onClick={onIgnoreUserClick}
+        >
+          <i
+            className={
+              isIgnored ? "fa fa-microphone" : "fa fa-microphone-slash"
+            }
+          ></i>
+        </button>
+      </Tooltip>
     </div>
   );
 };
